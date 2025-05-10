28e3ec519a641d6c3cea562d0badfd17814e92bd
Egor Ivanov <egor.i.ivanov@gmail.com>
2025-05-10 18:50:47 +0200

    program change

--- src/ConsoleClient/Program.cs
+++ src/ConsoleClient/Program.cs
@@ -1,5 +1,30 @@
-﻿using System.Security.Cryptography.X509Certificates;
+﻿using ConsoleClient;
+using Microsoft.Extensions.Configuration;
 
-X509Certificate2 agentCertificate = new X509Certificate2("AgentCertificate.pfx", "12345678");
+class Program
+{
+    static async Task Main()
+    {
+        var config = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: false)
+            .Build();
 
-Console.WriteLine(agentCertificate.Subject);
+        var section = config.GetSection("CesEnrollment");
+        string uri = section["Uri"];
+        string username = section["Username"];
+        string password = section["Password"];
+        string templateName = section["TemplateName"];
+        string csrPath = section["CsrPath"];
+        string outputPath = section["OutputPath"];
+
+        var csrBytes = File.ReadAllBytes(csrPath);
+        var csrBase64 = Convert.ToBase64String(csrBytes);
+
+        var client = new CesEnrollmentClient(uri, username, password);
+        var cert = await client.GetCertificateAsync(csrBase64, templateName);
+        File.WriteAllBytes(outputPath, cert);
+
+        Console.WriteLine($"Certificate saved to {outputPath}");
+    }
+}
